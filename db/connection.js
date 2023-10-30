3a8ac224334629a327b65eca2413b067aa552134
dislas03 <islas0903@gmail.com>
2023-10-30 00:51:43 -0700

    Fixed connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 
-const DB_URL = "mongodb+srv://islas0903:meliDavid09031106@cluster0.ir0olgy.mongodb.net/WAD-Dashers?retryWrites=true&w=majority"
+const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ir0olgy.mongodb.net/WAD-Dashers?retryWrites=true&w=majority`
 
 const PORT = process.env.PORT || 3000;
 
